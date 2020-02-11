2aa2282195b2dba82cc0285a9e9926de8f94e0d5
Nozomu Hirata <nonno.adobe.cc01@gmail.com>
2020-02-11 16:45:50 +0900

    base

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
 
 import './index.css';
 import reducer from './reducers';
-import App from './components/App';
-const store = createStore(reducer);
+import EventsIndex from './components/events_index';
+const store = createStore(reducer, applyMiddleware(thunk));
 // import registerServiceWorker from './registerServiceWorker';
 
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
 
   document.getElementById('root')
